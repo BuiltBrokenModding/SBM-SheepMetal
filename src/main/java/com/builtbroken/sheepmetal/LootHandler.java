309c94cfe4a3f075faa2b10f075f951f701fd871
Robert WC Seifert <rseifert.phone@gmail.com>
2019-06-15 19:03:33 -0400

    Disabled old loot table code

--- src/main/java/com/builtbroken/sheepmetal/LootHandler.java
+++ src/main/java/com/builtbroken/sheepmetal/LootHandler.java
@@ -2,15 +2,16 @@ package com.builtbroken.sheepmetal;
 
 import net.minecraft.world.storage.loot.LootEntryItem;
 import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
+import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 7/24/2018.
  */
-@Mod.EventBusSubscriber(modid = SheepMetal.DOMAIN)
+//@Mod.EventBusSubscriber(modid = SheepMetal.DOMAIN)
 public class LootHandler
 {
     @SubscribeEvent
@@ -25,7 +26,8 @@ public class LootHandler
                 LootPool lootPool = event.getTable().getPool(VANILLA_LOOT_POOL_ID);
                 if (lootPool != null)
                 {
-                    lootPool.addEntry(new LootEntryItem(type.woolItem, 1, 3, null, null, SheepMetal.PREFIX + "wool"));
+                    lootPool.addEntry(new LootEntryItem(type.woolItem, 1, 3,
+                            new LootFunction[0], new LootCondition[0], SheepMetal.PREFIX + "wool"));
                 }
             }
         }
